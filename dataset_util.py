35b10a974ff4b47f24bc8e6ec934571040e70380
Ji Liu <liuji1031@gmail.com>
2024-07-07 16:02:41 -0400

    started custom nuscene dataset class for pytorch

--- dataset_util.py
+++ dataset_util.py
@@ -4,10 +4,13 @@ from numpy.linalg import multi_dot
 from copy import copy, deepcopy
 from pathlib import Path
 import matplotlib.pyplot as plt
+import torch.utils.data as data
+import torch
+from loguru import logger
 from pyquaternion import Quaternion
+from easydict import EasyDict as edict
 from nuscenes.nuscenes import NuScenes
-from nuscenes.utils.data_classes import LidarPointCloud
-
+from nuscenes.utils.data_classes import LidarPointCloud, Box
 from nuscenes.utils.geometry_utils import transform_matrix
 
 def get_lidar_pts(nusc,sample):
@@ -149,7 +152,7 @@ def get_lidar_pts_multisweep(scene,
                 tmp.append(data)
             data_out.append(tmp)
             sample_tokens.append(sample_token)
-            break
+            # break
 
         sample_token = sample["next"]
 
@@ -182,3 +185,115 @@ def prev_sample(nusc,sample,skip=1):
         if sample["prev"] != "":
             sample = nusc.get("sample", sample["prev"])
     return sample
+
+def parameterize_gt_boxes(box : Box):
+    x,y,z = box.center
+    l,w,h = box.wlh
+    yaw = box.orientation.yaw_pitch_roll[0]
+    return edict(x=x,y=y,z=z,l=l,w=w,h=h,yaw=yaw)
+
+def get_gt_boxes(nusc : NuScenes, sample, target_class="vehicle"):
+    _, boxes, _ = nusc.get_sample_data(sample['data']['LIDAR_TOP'])
+    
+    annotations = sample["anns"]
+    assert(len(annotations) == len(boxes))
+
+    gt_boxes = []
+
+    for k,ann in enumerate(annotations):
+        ann = nusc.get('sample_annotation', ann)
+        # print(k,ann)
+        class_name = ann["category_name"].split(".")[0]
+        print(class_name)
+        if class_name == target_class:
+            gt_boxes.append(parameterize_gt_boxes(boxes[k]))
+
+    return gt_boxes
+
+def gt_boxes_to_array(gt_boxes):
+    """save gt_boxes to numpy array; each key mapped to a single numpy array
+
+    Args:
+        gt_boxes (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    x = []
+    y = []
+    z = []
+    l = []
+    w = []
+    h = []
+    yaw = []
+    for b in gt_boxes:
+        b : edict
+        x.append(b.x)
+        y.append(b.y)
+        z.append(b.z)
+        l.append(b.l)
+        w.append(b.w)
+        h.append(b.h)
+        yaw.append(b.yaw)
+    
+    # convert to numpy array
+    x = np.array(x)
+    y = np.array(y)
+    z = np.array(z)
+    l = np.array(l)
+    w = np.array(w)
+    h = np.array(h)
+    yaw = np.array(yaw)
+
+    return x,y,z,l,w,h,yaw
+
+class NusceneDataset(data.Dataset):
+
+    def __init__(self,path,
+                 canvas_h,
+                 canvas_w,
+                 anchors,
+                 target_class="vehicle",
+                 pos_neg_iou_threh=[0.6,0.45],
+                 version="v1.0-mini", ) -> None:
+        super().__init__()
+
+        self.nusc = NuScenes(version=version,dataroot=path,verbose=True)
+
+        self.canvas_h = canvas_h
+        self.canvas_w = canvas_w
+        self.anchors = anchors # list of 3d anchor boxes
+        self.target_class = target_class
+        self.pos_neg_iou_threh = pos_neg_iou_threh
+
+        self.n_anchor = len(anchors)
+
+        # go through all scenes and get all the sample tokens
+        self.sample_tokens = []
+        for scene in self.nusc.scene:
+            sample_token = scene["first_sample_token"]
+            while sample_token != "":
+                self.sample_tokens.append(sample_token)
+                sample_token = self.nusc.get("sample",sample_token)["next"]
+
+        self.n_samples = len(self.sample_tokens)
+        logger.info(f"Total number of samples: {self.n_samples}")
+
+    def __len__(self):
+        return self.n_samples
+    
+    def __getitem__(self, ind):
+
+        sample_token = self.sample_tokens[ind]
+
+        # get 3D bounding box
+        gt_boxes = get_gt_boxes(self.nusc,self.nusc.get("sample",sample_token),
+                                self.target_class)
+        x,y,z,l,w,h,yaw = gt_boxes_to_array(gt_boxes)
+
+        # from canvas and anchors, calculate iou's
+
+        
+        
+    def cal_target(self, ):
+        pass
